f1f716d30b86aee825a7797403a867ed58300c1a
Bruno Braga <brunobragaw8t@gmail.com>
2022-10-15 21:07:33 +0100

    Setup basic options

---
+++ lua/brunobragaw8t/options.lua
@@ -0,0 +1,5 @@
+vim.opt.cursorline = true     -- Highlight current line
+vim.opt.number = true         -- Display line numbers
+vim.opt.relativenumber = true -- Set line numbers as relative
+vim.opt.scrolloff = 999       -- Keep cursor centered on screen
+vim.opt.mouse = ''            -- Disable mouse
