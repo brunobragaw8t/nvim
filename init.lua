53819a5620c8ddacbc7be23e4a0b3062a047f16f
Bruno Braga <brunobragaw8t@gmail.com>
2022-11-01 10:37:05 +0000

    Review everything done so far

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 require('brunobragaw8t.options')
 require('brunobragaw8t.keymaps')
-require('brunobragaw8t.plugins') -- unclear
+require('brunobragaw8t.plugins')
 require('brunobragaw8t.colorscheme')
-require('brunobragaw8t.cmp') -- unclear
-require('brunobragaw8t.lsp') -- unclear
-require('brunobragaw8t.telescope') -- unclear
-require('brunobragaw8t.treesitter') -- unclear
-require('brunobragaw8t.autopairs') -- unclear
-require('brunobragaw8t.comment') -- unclear
-require('brunobragaw8t.gitsigns') -- unclear
+require('brunobragaw8t.cmp')
+require('brunobragaw8t.lsp')
+require('brunobragaw8t.telescope')
+require('brunobragaw8t.treesitter')
+require('brunobragaw8t.autopairs')
+require('brunobragaw8t.comment')
+require('brunobragaw8t.gitsigns')
 require('brunobragaw8t.nvim-tree')
-require('brunobragaw8t.bufferline') -- unclear
+require('brunobragaw8t.bufferline')
 require('brunobragaw8t.lualine')
--- lua/brunobragaw8t/telescope.lua
+++ lua/brunobragaw8t/telescope.lua
@@ -25,5 +25,4 @@ telescope.setup {
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<Leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<Leader>F', builtin.live_grep, {})
-vim.keymap.set('n', '<Leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<Leader>g', builtin.git_status, {})
