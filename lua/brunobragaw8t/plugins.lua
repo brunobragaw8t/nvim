eb896c11b7d56e9f7671ad392b8cff26f5c30418
Bruno Braga <brunobragaw8t@gmail.com>
2022-11-14 21:01:08 +0000

    Remove non-working media_files from Telescope

--- lua/brunobragaw8t/plugins.lua
+++ lua/brunobragaw8t/plugins.lua
@@ -24,7 +24,6 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
-  use "nvim-telescope/telescope-media-files.nvim"
 
   -- nvim-tree
   use "kyazdani42/nvim-tree.lua" -- File tree
--- lua/brunobragaw8t/telescope.lua
+++ lua/brunobragaw8t/telescope.lua
@@ -3,8 +3,6 @@ if not status_ok then
   return
 end
 
-telescope.load_extension("media_files")
-
 local actions = require("telescope.actions")
 
 telescope.setup({
@@ -16,9 +14,6 @@ telescope.setup({
         ["<Esc>"] = actions.close,
       },
     },
-  },
-  extensions = {
-    filetypes = {"png", "webp", "jpg", "jpeg"},
   },
 })
 
