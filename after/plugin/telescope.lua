56594c66b066216940aae718e19be626ae5e4a8f
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-04 08:48:24 +0000

    Fix coding style on some plugins

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,16 +1,17 @@
+local telescope = require("telescope")
 local actions = require("telescope.actions")
-
-require("telescope").setup{
-	defaults = {
-		mappings = {
-			i = {
-				["<Esc>"] = actions.close,
-			},
-		},
-	},
-}
-
 local builtin = require("telescope.builtin")
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<Esc>"] = actions.close,
+      },
+    },
+  },
+})
+
 vim.keymap.set("n", "<Leader>ff", builtin.find_files)
 vim.keymap.set("n", "<C-p>", builtin.git_files)
 vim.keymap.set("n", "<leader>fg", builtin.live_grep)
