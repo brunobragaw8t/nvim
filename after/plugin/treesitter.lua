452e2c38176b0cd35448b041a3aad05df0caab65
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-22 20:59:40 +0000

    Add preinstalled parsers to Treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,18 @@
 require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "css",
+    "help",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "php",
+    "scss",
+    "typescript",
+    "vim",
+    "vue",
+  },
 	highlight = {
 		enable = true,
 	},
