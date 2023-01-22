bf769c76c71704d4d94f054d26d8ac59a4dc05b2
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-22 18:54:44 +0000

    New keymap: clear search highlight on Escape

--- lua/brunobragaw8t/keymaps.lua
+++ lua/brunobragaw8t/keymaps.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 
 -- Don't yank on paste
 vim.keymap.set("v", "p", '"_dP')
+
+-- Clear search highlight on Escape
+vim.keymap.set("n", "<Esc>", ":nohls<CR>", { silent = true })
