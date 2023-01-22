3313736caf0c20071e977836670f53bfdcef9671
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-22 19:18:19 +0000

    New keymap: save with Ctrl+S

--- lua/brunobragaw8t/keymaps.lua
+++ lua/brunobragaw8t/keymaps.lua
@@ -6,3 +6,7 @@ vim.keymap.set("v", "p", '"_dP')
 
 -- Clear search highlight on Escape
 vim.keymap.set("n", "<Esc>", ":nohls<CR>", { silent = true })
+
+-- Save with Ctrl+S
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { silent = true })
+vim.keymap.set("n", "<C-s>", ":w<CR>", { silent = true })
