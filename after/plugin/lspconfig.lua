fb8daae63c84c3dca95d38efd08b0023c3ff4067
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-23 20:16:49 +0000

    Remove unnecessary keymaps from CMP

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -81,8 +81,6 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-e>"] = cmp.mapping.abort(),
