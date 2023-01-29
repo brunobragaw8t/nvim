7d5006345a413863aea788b30d188f9679f557a7
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-29 22:00:34 +0000

    Highlight text on yank

--- lua/brunobragaw8t/keymaps.lua
+++ lua/brunobragaw8t/keymaps.lua
@@ -13,3 +13,13 @@ vim.keymap.set({"i", "n", "v", "x"}, "<C-s>", vim.cmd.write, { silent = true })
 -- Move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
