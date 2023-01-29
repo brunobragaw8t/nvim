3c71e2035f3505cc616bf4e9d2a7965ecfa74858
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-29 20:52:57 +0000

    Change GitStatus from Telescope to Fugitive

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -15,4 +15,3 @@ telescope.setup({
 vim.keymap.set("n", "<Leader>ff", builtin.find_files)
 vim.keymap.set("n", "<C-p>", builtin.git_files)
 vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-vim.keymap.set("n", "<leader>gs", builtin.git_status)
