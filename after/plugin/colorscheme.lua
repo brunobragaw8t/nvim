dcb2bdd0d547e93267bd8ba2585ac7291c795209
Bruno Braga <brunobragaw8t@gmail.com>
2023-03-12 09:43:44 +0000

    Reconfigure colorscheme regarding updates

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,3 +1,9 @@
+vim.o.background = "dark"
+
+require("vscode").setup({
+  transparent = true,
+})
+
 local colorscheme = "vscode"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
@@ -7,6 +13,6 @@ if not status_ok then
 end
 
 -- Make background transparent
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
