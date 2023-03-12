09730d115392637b867f455be0572a55ed9a7ba2
Bruno Braga <brunobragaw8t@gmail.com>
2023-03-12 09:44:12 +0000

    Hide error on opening CRLF files

--- lua/brunobragaw8t/options.lua
+++ lua/brunobragaw8t/options.lua
@@ -11,3 +11,4 @@ vim.opt.tabstop = 2               -- Set tab width to 2 spaces
 vim.opt.expandtab = true          -- Convert tabs to spaces
 vim.opt.undofile = true           -- Save undo history for closed files
 vim.opt.colorcolumn = "80,120"    -- Set line-length rulers
+vim.opt.modeline = false
