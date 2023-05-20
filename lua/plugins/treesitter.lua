aedfe5c2f7885b4c27f0bf890cfdb23f3dd660b2
Bruno Braga <brunobragaw8t@gmail.com>
2023-05-20 19:31:33 +0100

    feature: install TreeSitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        "css",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "php",
+        "scss",
+        "typescript",
+        "vim",
+        "vue",
+      },
+      highlight = {
+        enable = true,
+      },
+      textobjects = {
+        swap = {
+          enable = true,
+          swap_next = {
+            ['<Leader>a'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<Leader>A'] = '@parameter.inner',
+          },
+        },
+      },
+    })
+  end,
+}
