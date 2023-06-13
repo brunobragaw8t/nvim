00ea494579519c56a40720cca9d215cc18e865c6
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-13 21:34:32 +0100

    feat: install indentline

---
+++ lua/plugins/indentline.lua
@@ -0,0 +1,10 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  commit = "8299fe7703dfff4b1752aeed271c3b95281a952d",
+  event = "BufReadPre",
+  opts = {
+    show_trailing_blankline_indent = false,
+    use_treesitter = true,
+    show_current_context = true,
+  }
+}
