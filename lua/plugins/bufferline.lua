cb325d44505d49130e13d79aabedd108ae67d1b6
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-13 20:40:06 +0100

    feat: hide buffer icons

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -14,6 +14,7 @@ return {
             text_align = "center",
           }
         },
+        show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
       },
