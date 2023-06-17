095aefc2acae841665a92214cf76f50c866860f3
Bruno Braga <brunobragaw8t@gmail.com>
2023-06-17 11:40:52 +0100

    feature: set transparent color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,10 @@ return {
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require("vscode").setup({})
+    require("vscode").setup({
+      transparent = true,
+    })
+
     vim.cmd[[colorscheme vscode]]
   end
 }
