b15f47788eb44309ae8b3b279a01086145f5dc9d
Bruno Braga <brunobragaw8t@gmail.com>
2023-07-16 19:07:08 +0100

    feature: auto trigger Copilot and change accept keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,5 +3,14 @@ return {
   commit = "686670843e6f555b8a42fb0a269c1bbaee745421",
   cmd = 'Copilot',
   event = "InsertEnter",
-  config = true,
+  config = function ()
+    require('copilot').setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<M-a>",
+        },
+      },
+    })
+  end,
 }
