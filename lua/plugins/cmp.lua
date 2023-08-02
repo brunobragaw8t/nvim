d3d8ab4016a98c55cf799f4c7d95908768cc9331
Bruno Braga <brunobragaw8t@gmail.com>
2023-08-02 21:12:35 +0100

    feature: add keymap to open completion dropdown

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -54,6 +54,7 @@ return {
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-n>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
