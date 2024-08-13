7af6d1acbb4de9263c527a49218eb45ee0138ef3
Bruno Braga <brunobragaw8t@gmail.com>
2024-08-13 20:05:29 +0100

    feature: disable unused plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+  { "garymjr/nvim-snippets", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+}
