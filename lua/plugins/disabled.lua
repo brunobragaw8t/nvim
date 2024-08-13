7f01c98c1c0cd11ac0f8a64a47add1fff676a7f8
Bruno Braga <brunobragaw8t@gmail.com>
2024-08-13 22:08:53 +0100

    feature: reenable neo-tree and customize it

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,6 @@
 return {
   { "garymjr/nvim-snippets", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
   { "folke/trouble.nvim", enabled = false },
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      width = 0,
+      auto_expand_width = true,
+    },
+    event_handlers = {
+      {
+        event = "file_open_requested",
+        handler = function()
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+    },
+  },
+}
