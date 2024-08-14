60810963ecefa20353ee83c8ae421ed4f7f8b804
Bruno Braga <brunobragaw8t@gmail.com>
2024-08-14 21:14:22 +0100

    feature: setup copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = "<M-a>",
+      },
+    },
+  },
+}
