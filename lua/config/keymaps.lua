51b9e646276bcc49c439a24191821c78eac15c3d
Bruno Braga <brunobragaw8t@gmail.com>
2024-08-15 11:35:45 +0100

    fix: remove conflict with default neovim keymap to delete buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,6 +13,8 @@ vim.keymap.set("n", "<C-Q>j", "<C-W>j")
 vim.keymap.set("n", "<C-Q>h", "<C-W>h")
 
 -- Close buffer
+vim.keymap.del("n", "<C-W>d") -- default Neovim keymap conflicting with mine
+vim.keymap.del("n", "<C-W><C-D>") -- default Neovim keymap conflicting with mine
 vim.keymap.set("n", "<C-w>", "<Cmd>bd<CR>")
 
 -- Don't yank on pasting
