f0629cd914d0274902e8e9ca134afe7b3f82c306
Bruno Braga <brunobragaw8t@gmail.com>
2024-12-29 17:59:48 +0000

    feat(file-tree): install plugin

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      git = {
+        enable = false,
+      },
+      view = {
+        adaptive_size = true,
+      },
+      update_focused_file = {
+        enable = true,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+      renderer = {
+        root_folder_label = false,
+      },
+    })
+  end,
+}
