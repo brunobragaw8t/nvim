546c04d29a91a14cf8e0f6f3e54725747c183ca6
Bruno Braga <brunobragaw8t@gmail.com>
2025-01-04 15:23:05 +0000

    feat(completion): install plugin with LSP source

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,33 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = { "InsertEnter" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-n>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+      },
+      window = {
+        completion = cmp.config.window.bordered({
+          border = "rounded",
+        }),
+        documentation = cmp.config.window.bordered({
+          border = "rounded",
+        }),
+      },
+    })
+  end,
+}
