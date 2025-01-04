e188d806ce527caa24ac9e07f9cbc27e66fd979c
Bruno Braga <brunobragaw8t@gmail.com>
2025-01-04 16:36:22 +0000

    feat(dashboard): install plugin

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,43 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = { "echasnovski/mini.icons" },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.opts.opts.noautocmd = true
+
+    dashboard.section.header.opts.hl = "Include"
+    dashboard.section.header.val = {
+      [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+      [[⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣀⣀⢠⣤⣶⣶⣾⣿⣿⣿⣿⣿⣿⣷⣶⣶⣤⡄⣀⣀⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀]],
+      [[⠀⠀⠀⠀⣸⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣇⠀⠀⠀⠀]],
+      [[⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀]],
+      [[⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀]],
+      [[⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀]],
+      [[⠀⠀⠈⢻⣿⣿⣿⣿⡿⠛⠛⠛⠛⠛⠛⠛⠛⠋⠉⠀⠀⠉⠙⠛⠛⠛⠛⠛⠛⠛⠛⢿⣿⣿⣿⣿⡟⠁⠀⠀]],
+      [[⠀⠀⢰⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⡄⠀⠀]],
+      [[⠀⢠⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⣿⣿⣿⣿⣿⣷⣤⣀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⡄⠀]],
+      [[⢠⣿⣿⣿⣿⣿⣿⣷⣤⣀⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣀⣤⣾⣿⣿⣿⣿⣿⣿⡀]],
+      [[⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷]],
+      [[⠈⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁]],
+      [[⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀]],
+      [[⠀⠀⠀⠀⠀⠀⠈⠙⢻⣿⣿⣿⠟⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠻⣿⣿⣿⡟⠋⠁⠀⠀⠀⠀⠀⠀]],
+      [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⡿⠀⠀⠀      ]],
+    }
+
+    dashboard.section.buttons.opts.hl = "Keyword"
+    dashboard.section.buttons.val = {
+      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    }
+
+    dashboard.section.footer.val = "Bruno Braga"
+    dashboard.section.footer.opts.hl = "Type"
+
+    alpha.setup(dashboard.opts)
+  end,
+}
