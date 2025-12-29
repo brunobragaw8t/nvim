bb0fb6a6c51a7700bea6738c600a47d5425ce7bc
Bruno Braga <brunobragaw8t@gmail.com>
2025-12-29 22:00:13 +0000

    feat(lsp): add TypeScript language server

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -115,6 +115,7 @@ return {
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       -- ts_ls = {},
 
+      ts_ls = {},
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
