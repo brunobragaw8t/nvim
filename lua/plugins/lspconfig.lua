fb64478e743b02c3fe9e4f2e51ae3a4ed158064a
Bruno Braga <brunobragaw8t@gmail.com>
2026-01-05 09:56:48 +0000

    chore(lsp): ensure installation of vue_ls and vtsls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -132,6 +132,8 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       "stylua", -- Used to format Lua code
+      "vue_ls",
+      "vtsls",
     })
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
