a936689c895abe7dd5cb7a80781d163f525dbf9f
Bruno Braga <brunobragaw8t@gmail.com>
2026-01-05 09:56:22 +0000

    fix(lsp): remove ts_ls as it's duplicated by vtsls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -109,13 +109,6 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-
-      ts_ls = {},
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
