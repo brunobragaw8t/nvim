89fae1a16b4527a367aaac41e61616e4267ad4d3
Bruno Braga <brunobragaw8t@gmail.com>
2026-03-22 10:36:26 +0000

    feat(git): add keymaps for git hunk interaction

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,13 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {},
+  config = function(_, opts)
+    local gs = require("gitsigns")
+    gs.setup(opts)
+
+    vim.keymap.set({ "n", "v" }, "<Leader>gs", gs.stage_hunk, { desc = "Stage hunk" })
+    vim.keymap.set({ "n", "v" }, "<Leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+    vim.keymap.set("n", "<Leader>gb", gs.toggle_current_line_blame, { desc = "Toggle blame line" })
+    vim.keymap.set("n", "<Leader>gx", gs.toggle_deleted, { desc = "Toggle deleted" })
+  end,
 }
