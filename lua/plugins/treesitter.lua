a2606fe4efaadaf55a02f59aa3cfa0f2830aedfa
Bruno Braga <brunobragaw8t@gmail.com>
2026-08-03 21:36:19 +0100

    feat(treesitter): migrate treesitter to new version and update other packages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,42 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  lazy = false,
   build = ":TSUpdate",
-  main = "nvim-treesitter.configs", -- Sets main module to use for opts
-  -- `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "css",
-      "diff",
-      "html",
-      "lua",
-      "luadoc",
-      "markdown",
-      "markdown_inline",
-      "query",
-      "vim",
-      "vimdoc",
-      "javascript",
-      "typescript",
-      "vue",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby" },
-    },
-    indent = { enable = true, disable = { "ruby" } },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        node_incremental = "<M-o>",
-        node_decremental = "<M-i>",
-      },
-    },
-  },
 }
